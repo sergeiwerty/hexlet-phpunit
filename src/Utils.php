728e8b195c40544cd80a31968009f8ea2a1b006a
Sergey <debrecin@yandex.ru>
2021-10-11 08:48:15 +0300

    test use case

--- src/Utils.php
+++ src/Utils.php
@@ -5,5 +5,6 @@ namespace Hexlet\Phpunit\Utils;
 // Эта функция переворачивает переданную строку
 function reverseString($string)
 {
+    print_r('something');
     return implode(array_reverse(str_split($string)));
 }
