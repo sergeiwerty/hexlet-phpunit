012c28dbc4dfcf388eb29d4c6b0a81136026057c
sergeiwerty <dshl.srp@gmail.com>
2021-10-11 08:59:20 +0300

    set repo to initial condition

--- src/Utils.php
+++ src/Utils.php
@@ -5,6 +5,5 @@ namespace Hexlet\Phpunit\Utils;
 // Эта функция переворачивает переданную строку
 function reverseString($string)
 {
-    print_r('something');
     return implode(array_reverse(str_split($string)));
 }
